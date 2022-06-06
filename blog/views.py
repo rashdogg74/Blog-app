fa8857234a4845c5b7b4f448596c66eee6f118fa
rashdogg74 <pauloriordan74@gmail.com>
2022-06-06 09:18:25 +0000

    Add css and update styling and add template files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.all().order_by()
+    template_name = 'index.html'
+    paginate_by = 8
 
 
+``
