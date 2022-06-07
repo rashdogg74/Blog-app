c3dc13fb4d63ecb8738ec5c8f28216be25473d21
rashdogg74 <pauloriordan74@gmail.com>
2022-06-07 14:31:33 +0000

    Install all auth and copy directories and slightly update login page

--- rashit/settings.py
+++ rashit/settings.py
@@ -29,12 +29,20 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'blog',
     'django_summernote',
     'cloudinary',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT = '/'
+LOGOUT_REDIRECT = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
